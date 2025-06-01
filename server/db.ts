08f71aac5fe881844d1a925d2e63b0345240cb8c
Ravula Koushal Sai <149877318+koushalsai@users.noreply.github.com>
2025-06-01 23:58:20 +0530

    Revert "Fix Database Connection and Schema"
    This reverts commit 2bffa2a2b83872e4f3bee04fad101c019ef865ff.

--- server/db.ts
+++ server/db.ts
@@ -18,13 +18,4 @@ const pool = new Pool({
   }
 });
 
-// Test the connection
-pool.query('SELECT NOW()', (err, res) => {
-  if (err) {
-    console.error('Database connection error:', err);
-  } else {
-    console.log('Database connected successfully');
-  }
-});
-
 export const db = drizzle(pool, { schema });
