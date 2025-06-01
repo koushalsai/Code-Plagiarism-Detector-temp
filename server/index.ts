617630d340546b0af36be120a2320463e495bce1
Ravula Koushal Sai <149877318+koushalsai@users.noreply.github.com>
2025-06-01 23:47:05 +0530

    Fix TypedArray buffer error in logging middleware

--- server/index.ts
+++ server/index.ts
@@ -9,11 +9,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   const start = Date.now();
   const path = req.path;
-  let capturedJsonResponse: Record<string, any> | undefined = undefined;
+  let hasJsonResponse = false;
 
   const originalResJson = res.json;
   res.json = function (bodyJson, ...args) {
-    capturedJsonResponse = bodyJson;
+    hasJsonResponse = true;
     return originalResJson.apply(res, [bodyJson, ...args]);
   };
 
@@ -21,14 +21,9 @@ app.use((req, res, next) => {
     const duration = Date.now() - start;
     if (path.startsWith("/api")) {
       let logLine = `${req.method} ${path} ${res.statusCode} in ${duration}ms`;
-      if (capturedJsonResponse) {
-        logLine += ` :: ${JSON.stringify(capturedJsonResponse)}`;
+      if (hasJsonResponse) {
+        logLine += ` :: [JSON Response]`;
       }
-
-      if (logLine.length > 80) {
-        logLine = logLine.slice(0, 79) + "…";
-      }
-
       log(logLine);
     }
   });
